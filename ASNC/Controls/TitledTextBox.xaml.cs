d5e4e03ae018268554e40cb2b880f69387c9882f
DarkIrata <git@ipmix.de>
2023-06-11 15:52:54 +0200

    Removed unused code from TitledTextBox

--- ASNC/Controls/TitledTextBox.xaml.cs
+++ ASNC/Controls/TitledTextBox.xaml.cs
@@ -11,7 +11,7 @@ namespace ASNC.Controls
     {
 
         public static readonly DependencyProperty TitleProperty = DependencyProperty.Register(nameof(Title), typeof(string), typeof(TitledTextBox), new PropertyMetadata(null, OnTitleChanged));
-        public static readonly DependencyProperty TextProperty = DependencyProperty.Register(nameof(Text), typeof(string), typeof(TitledTextBox), new FrameworkPropertyMetadata(null, FrameworkPropertyMetadataOptions.BindsTwoWayByDefault, OnTextChanged));
+        public static readonly DependencyProperty TextProperty = DependencyProperty.Register(nameof(Text), typeof(string), typeof(TitledTextBox), new FrameworkPropertyMetadata(null, FrameworkPropertyMetadataOptions.BindsTwoWayByDefault));
         public static readonly DependencyProperty IsReadOnlyProperty = DependencyProperty.Register(nameof(IsReadOnly), typeof(bool), typeof(TitledTextBox), new PropertyMetadata(false, OnReadOnlyChanged));
         public static readonly DependencyProperty LowerBorderBrushProperty = DependencyProperty.Register(nameof(LowerBorderBrush), typeof(SolidColorBrush), typeof(TitledTextBox), new PropertyMetadata(new SolidColorBrush(Color.FromRgb(25, 118, 210)), OnLowerBorderBurshChanged));
 
@@ -42,17 +42,6 @@ namespace ASNC.Controls
         public TitledTextBox()
         {
             this.InitializeComponent();
-            //this.Input.TextChanged += this.Input_TextChanged;
-        }
-
-        private void Input_TextChanged(object sender, TextChangedEventArgs e) => this.Text = this.Input.Text;
-
-        private static void OnTextChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
-        {
-            if (d is TitledTextBox tb && e.NewValue is string s)
-            {
-                //tb.Input.Text = s;
-            }
         }
 
         private static void OnTitleChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
