1728cdf138642361f2ffbd51cdaf01dd1b003c4f
DarkIrata <git@ipmix.de>
2023-07-26 18:56:49 +0200

    Add more Exception Logging

--- ASNC/App.xaml.cs
+++ ASNC/App.xaml.cs
@@ -1,4 +1,7 @@
-﻿using System.Windows;
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+using System.Windows;
 using ASNC.Data;
 using ASNC.Services;
 using ASNC.ViewModels;
@@ -25,6 +28,7 @@ namespace ASNC
         protected override void OnStartup(StartupEventArgs e)
         {
             base.OnStartup(e);
+            this.SetupExceptionHandling();
             this.MainWindow = new ShellView() { DataContext = this.shellViewModel };
             this.MainWindow.Show();
 
@@ -32,6 +36,41 @@ namespace ASNC
             {
                 this.shellViewModel.OpenSettingsCommand.Execute(null);
             }
+        }
+
+        private void SetupExceptionHandling()
+        {
+            AppDomain.CurrentDomain.UnhandledException += (s, e) =>
+                this.LogUnhandledException((Exception)e.ExceptionObject, "AppDomain.CurrentDomain.UnhandledException");
+
+            DispatcherUnhandledException += (s, e) =>
+            {
+                this.LogUnhandledException(e.Exception, "Application.Current.DispatcherUnhandledException");
+                e.Handled = true;
+            };
+
+            TaskScheduler.UnobservedTaskException += (s, e) =>
+            {
+                this.LogUnhandledException(e.Exception, "TaskScheduler.UnobservedTaskException");
+                e.SetObserved();
+            };
+        }
+
+        private void LogUnhandledException(Exception exception, string source)
+        {
+            var timestamp = DateTime.Now.ToString("yyyy-dd-M_HH-mm-ss");
+            var filename = $"Error_{timestamp}.txt";
+            try
+            {
+                var message = $"Unhandled exception ({source}){Environment.NewLine}Type: {exception?.GetType().FullName}{Environment.NewLine}Message: {exception?.Message}{Environment.NewLine}StackTrace: {exception?.StackTrace}";
+                File.WriteAllText(filename, message);
+            }
+            catch (Exception ex)
+            {
+                File.WriteAllText(filename, ex.Message);
+            }
+
+            MessageBox.Show($"Unhandled exception occurred!{Environment.NewLine}Error: {exception?.Message}{Environment.NewLine}An error log was generated and named '{filename}'.", "Critical Error", MessageBoxButton.OK, MessageBoxImage.Error);
         }
     }
 }
